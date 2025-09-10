dd698d3f5b448edb189cf606735e6f8d6ee325f7
FilipovAnatolii <anatolii.filippov@icloud.com>
2025-09-10 21:16:41 +0000

    Deploying to gh-pages from @ FilipovAnatolii/goit-js-hw-09@3c2806ae9cbadecb58bed954b0c4b29f6a1820a1 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as a}from"./assets/vendor-CgTBfC_f.js";new a(".gallery a",{captionsData:"alt",captionDelay:250,animationSpeed:250,fadeSpeed:250,nav:!0,close:!0});
+//# sourceMappingURL=1-gallery.js.map
